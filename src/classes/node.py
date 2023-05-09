8aa91710bc6975bfeea4f5024c424584aaa669f7
Martin Renner <mrenner.mail@seznam.cz>
2023-05-09 18:40:12 +0200

    Defined methods of node.py

---
+++ src/classes/node.py
@@ -0,0 +1,16 @@
+class Node:
+    """
+    Node class
+    """
+
+    def __init__(self):
+        ...
+
+    def push(self):
+        ...
+
+    def isEmpty(self):
+        ...
+
+    def __len__(self):
+        ...
