e47b1bcaa1205acece464674efdeaf5d9796b425
Martin Renner <mrenner.mail@seznam.cz>
2023-05-11 10:27:59 +0200

    Implemented methods of home.py

--- src/classes/home.py
+++ src/classes/home.py
@@ -1,8 +1,21 @@
 from node import Node
 
 class Home(Node):
-    def __init__(self):
-        pass
+    def __init__(self, sumOfAllStones = 15):
+        """
+        Constructor - initialize home
+
+        Parameters:
+        sumOfAllStones (Int): number of stones that determine end of game
+        """
+        super().__init__()
+        self.sumOfAllStones = sumOfAllStones
 
     def allStonesHome(self):
-        pass
+        """
+        AllStonesHome - check if all stones are home
+
+        Returns: 
+        Bool - False unless all stones are home, otherwise True
+        """
+        return False if len(self.stones) < self.allStonesHome else True  
